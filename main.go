dc3b38ee699c7c95e00c00c7e668cfa454ca960e
lili10000 <476512727@qq.com>
2020-01-17 18:37:47 +0800

    1

--- main.go
+++ main.go
@@ -50,37 +50,49 @@ func checkBig(input []float64) int{
     return maxIndex
 }
 
+func dataChange(input float64) float64 {
+	tmp := 1/(math.Exp(-1*input) + 1)
+	return tmp
+}
 
 func main()  {
 
-    dataList := getTrainData("train.txt")
+    dataList := getTrainData("today.txt")
+
+    dataTest := dataList[:len(dataList)/2]
+    dataTrain := dataList[len(dataList)/2:]
+
 
     var inNode InNodeMgr
     var bNode BNodeMgr
     var bNode_1 BNodeMgr
     var yNode YNodeMgr
-    inNode.InitInNodeMgr(4)
+    inNode.InitInNodeMgr(5)
 
-    bNode.InitBNodeMgr(5)
+    bNode.InitBNodeMgr(6)
     bNode.SetInNode(&inNode)
 
-    bNode_1.InitBNodeMgr(6)
+    bNode_1.InitBNodeMgr(7)
     bNode_1.SetBNode(&bNode)
 
     yNode.InitYNodeMgr(1)
     yNode.SetBNode(&bNode_1)
 
-    for i:= 0; i < 10000; i++{
-        for _, data := range dataList{
-            if len(data) != 5{
+    for i:= 0; i < 1000; i++{
+        for _, data := range dataTrain{
+            if len(data) != 7{
                 break
             }
 
-            InData := data[:len(data)-1]
-            resultTmp := data[len(data)-1]
+            InData := data[:len(data)-2]
+            
 
-            resultTmp = resultTmp*0.3
-            result := []float64{resultTmp}
+            rate := data[len(data)-2]
+            rate = dataChange(rate)
+            score := data[len(data)-1]
+            score = dataChange(score)
+
+            result := []float64{rate}
 
     
             inNode.DoInput(InData)
@@ -108,19 +120,27 @@ func main()  {
         }
     }
 
-    dataList = getTrainData("test.txt")
+    // dataList = getTrainData("test.txt")
 
     okSum := 0
     errSum := 0
-    for _, data := range dataList{
-        if len(data) != 5{
+    for _, data := range dataTest{
+        if len(data) != 7{
             break
         }
-        InData := data[:len(data)-1]
-        resultTmp := data[len(data)-1]
+        InData := data[:len(data)-2]
 
-        resultTmp = resultTmp*0.3
-        result := []float64{resultTmp}
+        rate := data[len(data)-2]
+        rate = dataChange(rate)
+        score := data[len(data)-1]
+        score = dataChange(score)
+
+        result := []float64{rate}
+
+        // resultTmp := data[len(data)-1]
+
+        // resultTmp = resultTmp*0.3
+        // result := []float64{resultTmp}
 
         inNode.DoInput(InData)
         inNode.DoSend()
@@ -128,10 +148,12 @@ func main()  {
         bNode_1.DoSend()
         Y_get := yNode.GetResult_Y()
 
-        getIndex := math.Abs(result[0] - Y_get[0]) <= 0.02
-        // fmt.Println("real:", result[0], "get:", Y_get[0], "result:", math.Abs(result[0] - Y_get[0]), getIndex )
+        // getIndex := (math.Pow((result[0] - Y_get[0]), 2) + math.Pow(math.Abs(result[1] - Y_get[1]), 2)) /2
+        // fmt.Println("0:", result[0], "0:", Y_get[0],)
+        // fmt.Println("1:", result[1], "1:", Y_get[1],)
+        getIndex := ((result[0] < 0.5) && (Y_get[0] < 0.5)) || ((result[0] > 0.5) && (Y_get[0] > 0.5))
 
-        if getIndex  {
+        if getIndex {
             okSum += 1
         }else{
             errSum  +=1
@@ -144,24 +166,4 @@ func main()  {
         
     }
     fmt.Println("ok:",okSum, "err:", errSum)
-
-
-
-    // var nodeList []float64
-    // result := []float64{0,1,0} 
-
-
-
-    // for i:= 0; i < 10; i++{
-    //     inNode.DoInput(nodeList)
-    //     inNode.DoSend()
-    //     bNode.DoSend()
-    //     Y_get := yNode.GetResult_Y()
-    //     fmt.Println(Y_get,)
-    //     B_get := bNode.GetResult_B()
-    //     B_weight := bNode.GetWeight_B()
-    //     yNode.DoDelta_Y(result)
-    //     bNode.DoDelta_B(result, Y_get)
-    //     inNode.DoDelta_In(result, Y_get, B_get, B_weight)
-    // }
 }
